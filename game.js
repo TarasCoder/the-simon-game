be113f384a0aa50411deceaa038c190069a9d0db
TarasCoder <4kaskad@gmail.com>
2021-11-25 23:14:17 +0200

    creating game and making some first settings

---
+++ game.js
@@ -0,0 +1,10 @@
+let gamePattern = [];
+let buttonColours = ["red", "blue", "green", "yellow"];
+
+function nextSequence() {
+  let randomNumber = Math.floor(Math.random() * 4);
+  let randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+}
+nextSequence();
+console.log(gamePattern);
