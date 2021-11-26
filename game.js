8e05d18974bac8027da206642961828d9f8fa248
TarasCoder <4kaskad@gmail.com>
2021-11-27 00:24:25 +0200

    adding animation and random first position to button

--- game.js
+++ game.js
@@ -5,6 +5,16 @@ function nextSequence() {
   let randomNumber = Math.floor(Math.random() * 4);
   let randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
+  selectButton(randomChosenColour);
+  playSound(randomChosenColour);
 }
 nextSequence();
-console.log(gamePattern);
+
+function selectButton(element){
+  $("." + element).fadeOut(100).fadeIn(100);
+}
+
+function playSound(el){
+  let audio = new Audio('./sounds/' + el + '.mp3');
+  audio.play();
+}
